4ab81ea851c599544394b2a359b3e7c224d97d34
Carlos Escobar <escobarcarlos@gmail.com>
2021-10-21 13:25:25 -0600

    Fixed-logic

--- src/Components/login_components/Dashboard.js
+++ src/Components/login_components/Dashboard.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { useHistory } from "react-router";
 import "./Dashboard.css";
-import { auth, db, logout } from "../firebase/firebase";
+import { auth, db, logout } from "../firebase/firebase"
 
 import {questions} from '../../data/dataQuestions'
 
@@ -24,71 +24,68 @@ function Dashboard() {
       alert("An error occured while fetching user data");
     }
   };
-  useEffect(() => {
-    if (loading) return;
-    if (!user) return history.replace("/");
-    fetchUserName();
-  }, [user, loading]);
-  
-  const[stateQuestion, setStateQuestion] = useState(0);
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+	
+	// Total score per house setters
+	const [totalGryffindor, setGryffindor] = useState(0);
+	const [totalSlytherin, setSlytherin] = useState(0);
+	const [totalRavenclaw, setRavenclaw] = useState(0);
+	const [totalHufflepuff, setHufflepuff] = useState(0);
 
-  const[totalpointsGryffindor, setGryffindorPoints] = useState(0)
-  const[totalpointsRavenClaw, setRavenclawPoints] = useState(0)
-  const[totalpointsSlytherin, setSyltherinPoints] = useState(0)
-  const[totalpointsHufflePuff, setHufflePuffPoints] = useState(0)
+	//Final result setter
+	const [house, setHouse] = useState("Muggle");
 
-  const[house, setHouse]=useState('Muggle')
+	const [showQuiz, setShowQuiz] = useState(false);
+	const [showHouse, setShowHouse] = useState(false);
+	
+	//Sorting function
+	const answerHandler = (gryffindor, slytherin, ravenclaw, hufflepuff) => {
+		setGryffindor(totalGryffindor + gryffindor);
+		setSlytherin(totalSlytherin + slytherin);
+		setRavenclaw(totalRavenclaw + ravenclaw);
+		setHufflepuff(totalHufflepuff + hufflepuff);
 
+		switch (Math.max(totalGryffindor, totalSlytherin, totalRavenclaw, totalHufflepuff)) {
+			case totalGryffindor: setHouse("Gryffindor");
+				break;
+			case totalSlytherin: setHouse("Slytherin");
+				break;
+			case totalRavenclaw: setHouse("Ravenclaw");
+				break;
+			case totalHufflepuff: setHouse("Hufflepuff");
+				break;
+			default:
+        console.log(gryffindor,slytherin,ravenclaw,hufflepuff)
+				break; 
+		};
 
-  const[showQuiz, setShowQuiz] = useState(false)
-  const[showHouse, setShowHouse] = useState(false)
-
-
-  const AnswerHandler = (gryffindor, ravenclaw, syltherin, hufflepuff) => { 
-    setGryffindorPoints(totalpointsGryffindor + gryffindor); 
-    setRavenclawPoints(totalpointsRavenClaw + ravenclaw); 
-    setSyltherinPoints(totalpointsSlytherin + syltherin);
-    setHufflePuffPoints(totalpointsHufflePuff + hufflepuff); 
-
-    switch(Math.max(totalpointsGryffindor, totalpointsRavenClaw, totalpointsSlytherin, totalpointsHufflePuff)) { 
-      case totalpointsGryffindor: setHouse('Gryffindor')
-          break;
-      case totalpointsRavenClaw: setHouse('RavenClaw')
-          break; 
-      case totalpointsSlytherin: setHouse('Slytherin')
-          break; 
-      case totalpointsHufflePuff: setHouse('HufflePuff')
-          break; 
-      default:
-        console.log('Not working bozo')
-  
-          break;
-    }
-    const nextQuestion = stateQuestion + 1; 
-    if(nextQuestion < questions.length) { 
-      setStateQuestion(nextQuestion);
-    } else  {
-        setShowHouse(true); 
-    };
-    console.log(AnswerHandler)
-  }
-
-    const[banner, setBanner] = useState('')
-	  const changeBackground = () => {
-		  if (house === 'Gryffindor' && showHouse===true) {
+		const nextQuestion = currentQuestion + 1;
+		if (nextQuestion < questions.length) {
+			setCurrentQuestion(nextQuestion);
+		} else {
+			setShowHouse(true);
+		}
+	};
+	
+	const [banner, setBanner] = useState('')
+	const changeBackground = () => {
+		if (house === 'Gryffindor' && showHouse===true) {
 			setBanner(require("../../Pictures/gryfinndorFlag.jpeg"));}
-		  if (house === 'RavenClaw' && showHouse===true) {
-			setBanner(require("../../Pictures/harry-potter-ravenclaw.jpeg"));}
-	  	if (house === 'Slytherin' && showHouse===true) {
+		if (house === 'Slytherin' && showHouse===true) {
 			setBanner(require("../../Pictures/Slytherin.jpeg"));}
-		  if (house === 'Hufflepuff' && showHouse===true) {
+		if (house === 'Ravenclaw' && showHouse===true) {
+			setBanner(require("../../Pictures/harry-potter-ravenclaw.jpeg"));}
+		if (house === 'Hufflepuff' && showHouse===true) {
 			setBanner(require("../../Pictures/HufflePuff.jpeg"));}
 	};
+	// Always check which house has most points in order to reveal the respective banner
+	useEffect(() => {
+		changeBackground()
+	});
 
-
-  return (
-    <div>
-		<div className='main-title'>Become Wizard</div>
+	return (
+		<div>
+		<div className='main-title'>Become a Wizard</div>
 		<div className='app' style={{borderRadius:'7px', backgroundPosition: '50%', backgroundBlendMode:'normal', backgroundImage: `url(${banner})`}}>
 			{showQuiz ? (
 			<div>
@@ -104,13 +101,13 @@ function Dashboard() {
 				<div className='part-two'>
 					<div className='question-section'>
 						<div className='question-count'>
-							<span>Question {stateQuestion + 1}</span>/{questions.length}
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
 						</div>
-						<div className='question-text'>{questions[stateQuestion].questionText}</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
 					</div>
 					<div className='answer-section'>
-						{questions[stateQuestion].options.map((option) => (
-							<button onClick={() => {AnswerHandler(option.gryffindor, option.slytherin, option.ravenclaw, option.hufflepuff);}}>{option.optionText}</button>
+						{questions[currentQuestion].options.map((option) => (
+							<button onClick={() => {answerHandler(option.gryffindor,option.slytherin, option.ravenclaw, option.hufflepuff);}}>{option.optionText}</button>
 						))}
 					</div>
 				</div>
@@ -121,7 +118,7 @@ function Dashboard() {
 		<>
 				<div className='intro-part'>
 					<div className='intro-text'>
-						Welcome answer the series of Questions to find where you truly belong
+					welcome
 					</div>
 					<button className='start-button button-loader' onClick={() => setShowQuiz(true)}>Start</button>
 				</div>
@@ -131,16 +128,112 @@ function Dashboard() {
 		</div>
 	);
 }
-    // <div className="dashboard">
-    //   <div className="dashboardHeader">
-    //       <h1>Become a Wizard</h1>
-    //  <div className="nameCss">{name}</div>
-    //   <button className="dashboard__btn" onClick={logout}>
-    //       Logout
-    //     </button>
-    //     </div>
-        
+  
+//   const[stateQuestion, setStateQuestion] = useState(0);
 
-    // </div>
+//   const[totalpointsGryffindor, setGryffindorPoints] = useState(0);
+//   const[totalpointsRavenClaw, setRavenclawPoints] = useState(0);
+//   const[totalpointsSlytherin, setSyltherinPoints] = useState(0);
+//   const[totalpointsHufflePuff, setHufflePuffPoints] = useState(0);
+
+//   const[house, setHouse]=useState('Muggle');
+
+
+//   const[showQuiz, setShowQuiz] = useState(false);
+//   const[showHouse, setShowHouse] = useState(false);
+
+
+//   const AnswerHandler = (gryffindor, ravenclaw, syltherin, hufflepuff) => { 
+//     setGryffindorPoints(totalpointsGryffindor + gryffindor); 
+//     setRavenclawPoints(totalpointsRavenClaw + ravenclaw); 
+//     setSyltherinPoints(totalpointsSlytherin + syltherin);
+//     setHufflePuffPoints(totalpointsHufflePuff + hufflepuff); 
+
+//     switch(Math.max(totalpointsGryffindor, totalpointsRavenClaw, totalpointsSlytherin, totalpointsHufflePuff)) { 
+//       case totalpointsGryffindor: setHouse("Gryffindor")
+//           break;
+//       case totalpointsRavenClaw: setHouse("RavenClaw")
+//           break; 
+//       case totalpointsSlytherin: setHouse("Slytherin")
+//           break; 
+//       case totalpointsHufflePuff: setHouse("HufflePuff")
+//           break; 
+//       default:
+  
+//           break;
+//     };
+//     const nextQuestion = stateQuestion + 1; 
+//     if(nextQuestion < questions.length) { 
+//       setStateQuestion(nextQuestion);
+//     } else  {
+//         setShowHouse(true); 
+//     };
+//   };
+//   useEffect(() => {
+//     if (loading) return;
+//     if (!user) return history.replace("/");
+//     fetchUserName();
+//     changeBackground();
+//   }, [user, loading]);
+
+//     const[banner, setBanner] = useState('')
+// 	  const changeBackground = () => {
+// 		  if (house === 'Gryffindor' && showHouse===true) {
+// 			setBanner(require("../../Pictures/gryfinndorFlag.jpeg"));}
+// 		  if (house === 'RavenClaw' && showHouse===true) {
+// 			setBanner(require("../../Pictures/harry-potter-ravenclaw.jpeg"));}
+// 	  	if (house === 'Slytherin' && showHouse===true) {
+// 			setBanner(require("../../Pictures/Slytherin.jpeg"));}
+// 		  if (house === 'Hufflepuff' && showHouse===true) {
+// 			setBanner(require("../../Pictures/HufflePuff.jpeg"));}
+// 	};
+
+
+//   return (
+//     <div>
+// 		<div className='main-title'>Become Wizard</div>
+// 		<div className='app' style={{borderRadius:'7px', backgroundPosition: '50%', backgroundBlendMode:'normal', backgroundImage: `url(${banner})`}}>
+// 			{showQuiz ? (
+// 			<div>
+// 			{showHouse ? (
+// 				<div className='score-section'>
+// 					You belong in 
+// 					<br/>
+// 					<p className='house'>{house}</p> 
+// 				</div>
+				
+// 			) : (
+// 				<>
+// 				<div className='part-two'>
+// 					<div className='question-section'>
+// 						<div className='question-count'>
+// 							<span>Question {stateQuestion + 1}</span>/{questions.length}
+// 						</div>
+// 						<div className='question-text'>{questions[stateQuestion].questionText}</div>
+// 					</div>
+// 					<div className='answer-section'>
+// 						{questions[stateQuestion].options.map((option) => (
+// 							<button onClick={() => {AnswerHandler(option.gryffindor, option.slytherin, option.ravenclaw, option.hufflepuff);}}>{option.optionText}</button>
+// 						))}
+// 					</div>
+// 				</div>
+					
+// 				</>
+// 			)} </div>
+// 		) : (
+// 		<>
+// 				<div className='intro-part'>
+// 					<div className='intro-text'>
+// 						Welcome answer the series of Questions to find where you truly belong
+// 					</div>
+// 					<button className='start-button button-loader' onClick={() => setShowQuiz(true)}>Start</button>
+// 				</div>
+// 		</>
+// 		)}
+// 		</div>
+// 		</div>
+// 	);
+// }
+  
 
 export default Dashboard;
--- src/data/dataQuestions.js
+++ src/data/dataQuestions.js
@@ -1,11 +1,12 @@
+
 export const questions = [ 
 { 
     questionText: 'A student decided to sneak out of the Castle at night and tells your to cover for them. What do you do?',
     options: [ 
-        {optionText:'Cover for them', griffindor:2, ravenclaw:2, slytherin:1, hufflepuff:2},
-        {optionText:'Tell the teacher', griffindor:0, ravenclaw:2, slytherin:4, hufflepuff:1},
-        {optionText:'Convince the student to stay', griffindor:3, ravenclaw:2, slytherin:0, hufflepuff:2},
-        {optionText:'Dont cover for them', griffindor:1, ravenclaw:1, slytherin:4, hufflepuff:1}
+        {optionText:'Cover for them', gryffindor:2, ravenclaw:2, slytherin:1, hufflepuff:2},
+        {optionText:'Tell the teacher', gryffindor:0, ravenclaw:1, slytherin:4, hufflepuff:1},
+        {optionText:'Convince the student to stay', gryffindor:1, ravenclaw:2, slytherin:0, hufflepuff:4},
+        {optionText:'Dont cover for them', gryffindor:0, ravenclaw:2, slytherin:4, hufflepuff:1}
 
 
     ],
@@ -13,10 +14,10 @@ export const questions = [
 {
     questionText: 'Your bully at school desides to challege you to a battle. Whats the first spell you would use on your bully?',
     options : [ 
-        {optionText:'Expelliarmus', griffindor:2, ravenclaw:2, slytherin:1, hufflepuff:2},
-        {optionText:'Crucio', griffindor:1, ravenclaw:1, slytherin:3, hufflepuff:1},
-        {optionText:'Stupefy', griffindor:1, ravenclaw:2, slytherin:1, hufflepuff:3},
-        {optionText:'Avada Kedvara', griffindor:0, ravenclaw:0, slytherin:4, hufflepuff:0},
+        {optionText:'Expelliarmus', gryffindor:2, ravenclaw:2, slytherin:0, hufflepuff:2},
+        {optionText:'Crucio', gryffindor:0, ravenclaw:0, slytherin:4, hufflepuff:0},
+        {optionText:'Stupefy', gryffindor:2, ravenclaw:3, slytherin:1, hufflepuff:1},
+        {optionText:'Avada Kedvara', gryffindor:0, ravenclaw:1, slytherin:4, hufflepuff:0}
 
 
 
@@ -26,79 +27,79 @@ export const questions = [
 {
     questionText: 'What do you believe is your strongest trait?',
     options : [ 
-        {optionText:'Loving', griffindor:1, ravenclaw:2, slytherin:1, hufflepuff:2},
-        {optionText:'Relentless', griffindor:3, ravenclaw:1, slytherin:3, hufflepuff:0},
-        {optionText:'patience', griffindor:1, ravenclaw:2, slytherin:0, hufflepuff:3},
-        {optionText:'intergrity', griffindor:3, ravenclaw:1, slytherin:1, hufflepuff:1},
-    ]
+        {optionText:'Loving', gryffindor:1, ravenclaw:1, slytherin:1, hufflepuff:2},
+        {optionText:'Relentless', gryffindor:2, ravenclaw:1, slytherin:3, hufflepuff:0},
+        {optionText:'patience', gryffindor:1, ravenclaw:2, slytherin:0, hufflepuff:3},
+        {optionText:'intergrity', gryffindor:2, ravenclaw:1, slytherin:0, hufflepuff:3}
+    ],
 },
 {
     questionText: 'Which Wizard inspire you most at Hogwarts',
     options : [ 
-        {optionText: 'Dumbledore', griffindor:3, ravenclaw: 2, slytherin:0, hufflepuff:2},
-        {optionText: 'Professor Snape', griffindor:0, ravenclaw: 2, slytherin:4, hufflepuff:0},
-        {optionText: 'Hagrid', griffindor:3, ravenclaw:2, slytherin:0, hufflepuff:2},
-        {optionText: 'Pomona Sprout', griffindor:0, ravenclaw: 2, slytherin:0, hufflepuff:4}
+        {optionText: 'Dumbledore', gryffindor:3, ravenclaw: 2, slytherin:0, hufflepuff:2},
+        {optionText: 'Professor Snape', gryffindor:0, ravenclaw: 1, slytherin:4, hufflepuff:0},
+        {optionText: 'Hagrid', gryffindor:3, ravenclaw:1, slytherin:0, hufflepuff:2},
+        {optionText: 'Pomona Sprout', gryffindor:0, ravenclaw: 2, slytherin:0, hufflepuff:4}
 
-    ]
+    ],
 },
 {
     questionText: 'Which item would you choose to have forever?',
     options : [ 
-        {optionText: 'ElderWand', griffindor:1, ravenclaw: 2, slytherin:3, hufflepuff:0},
-        {optionText: 'Cloak', griffindor:2, ravenclaw: 2, slytherin:1, hufflepuff:2},
-        {optionText: 'Stone', griffindor:0, ravenclaw:3, slytherin:1, hufflepuff:2},
-        {optionText: 'Time-Turner', griffindor:2, ravenclaw: 2, slytherin:4, hufflepuff:1}
+        {optionText: 'ElderWand', gryffindor:1, ravenclaw: 2, slytherin:3, hufflepuff:0},
+        {optionText: 'Cloak', gryffindor:2, ravenclaw: 1, slytherin:1, hufflepuff:3},
+        {optionText: 'Stone', gryffindor:0, ravenclaw:2, slytherin:1, hufflepuff:1},
+        {optionText: 'Time-Turner', gryffindor:2, ravenclaw: 2, slytherin:3, hufflepuff:1}
 
-    ]
+    ],
 },
 {
     questionText: 'What core would you choose for your wand?',
     options : [ 
-        {optionText: 'Unicorn-Hair', griffindor:2, ravenclaw: 1, slytherin:3, hufflepuff:4},
-        {optionText: 'Phoenix', griffindor:1, ravenclaw: 2, slytherin:3, hufflepuff:1},
-        {optionText: 'Dragon', griffindor:1, ravenclaw:3, slytherin:1, hufflepuff:1},
-        {optionText: 'Basilisk', griffindor:1, ravenclaw: 3, slytherin:3, hufflepuff:0}
+        {optionText: 'Unicorn-Hair', gryffindor:2, ravenclaw: 1, slytherin:1, hufflepuff:3},
+        {optionText: 'Phoenix', gryffindor:1, ravenclaw: 2, slytherin:2, hufflepuff:1},
+        {optionText: 'Dragon', gryffindor:1, ravenclaw:3, slytherin:1, hufflepuff:1},
+        {optionText: 'Basilisk', gryffindor:1, ravenclaw: 1, slytherin:3, hufflepuff:0}
 
-    ]
+    ],
 }, 
 {
     questionText: 'Which one of these four choices would you be more interested in working in after your time at Hogwarts',
     options : [ 
-        {optionText: 'teacher', griffindor:2, ravenclaw: 1, slytherin:1, hufflepuff:3},
-        {optionText: 'ministry', griffindor:0, ravenclaw: 3, slytherin:3, hufflepuff:1},
-        {optionText: 'Quidditch-Player', griffindor:2, ravenclaw:2, slytherin:2, hufflepuff:1},
-        {optionText: 'Oblivator', griffindor:0, ravenclaw: 3, slytherin:3, hufflepuff:0}
+        {optionText: 'teacher', gryffindor:2, ravenclaw: 1, slytherin:1, hufflepuff:3},
+        {optionText: 'ministry', gryffindor:0, ravenclaw: 3, slytherin:3, hufflepuff:1},
+        {optionText: 'Quidditch-Player', gryffindor:2, ravenclaw:1, slytherin:2, hufflepuff:1},
+        {optionText: 'Oblivator', gryffindor:0, ravenclaw: 2, slytherin:2, hufflepuff:0}
 
-    ]
+    ],
 },
 {
     questionText: 'What class would you most be interested at attending at HogWarts',
     options : [ 
-        {optionText: 'DefenceArts', griffindor:3, ravenclaw: 1, slytherin:3, hufflepuff:0},
-        {optionText: 'Charms', griffindor:2, ravenclaw: 2, slytherin:1, hufflepuff:1},
-        {optionText: 'Herbology', griffindor:2, ravenclaw:1, slytherin:0, hufflepuff:4},
-        {optionText: 'Potions', griffindor:1, ravenclaw: 3, slytherin:1, hufflepuff:2}
+        {optionText: 'DefenceArts', gryffindor:2, ravenclaw: 1, slytherin:2, hufflepuff:0},
+        {optionText: 'Charms', gryffindor:2, ravenclaw: 2, slytherin:1, hufflepuff:1},
+        {optionText: 'Herbology', gryffindor:2, ravenclaw:1, slytherin:0, hufflepuff:4},
+        {optionText: 'Potions', gryffindor:1, ravenclaw: 3, slytherin:1, hufflepuff:1}
 
-    ]
+    ],
 },
 {
     questionText: 'If you could take a luck potion to win a quidditch match, would you?',
     options: [
-        { optionText: 'Absolutely', gryffindor: 2, slytherin: 3, ravenclaw: 1, hufflepuff: 0 },
-        { optionText: 'Maybe', gryffindor: 1, slytherin: 1, ravenclaw: 3, hufflepuff: 1 },
-        { optionText: 'No', gryffindor: 2, slytherin: 0, ravenclaw: 2, hufflepuff: 3 },
-        { optionText: 'Quidditch sucks because I suck', gryffindor: 0, slytherin: 0, ravenclaw: 2, hufflepuff: 4},
+        { optionText: 'Absolutely', gryffindor:1, slytherin: 3, ravenclaw: 1, hufflepuff: 0 },
+        { optionText: 'Maybe', gryffindor: 1, slytherin: 1, ravenclaw: 2, hufflepuff: 1 },
+        { optionText: 'No', gryffindor: 1, slytherin: 0, ravenclaw: 0, hufflepuff: 3},
+        { optionText: 'Quidditch sucks because I suck', gryffindor: 0, slytherin: 0, ravenclaw: 2, hufflepuff: 2}
     ],
 },
 {
     questionText: 'Final question. imagine you are figthing the Death Eaters and Lord Voldermort purposes to those who stay will be spared. What would you do?',
     options: [
-        { optionText: 'Join him he\'s the most powerful', gryffindor: 0, slytherin: 4, ravenclaw: 2, hufflepuff: 2 },
-        { optionText: 'Probably join him to not die', gryffindor: 0, slytherin: 3, ravenclaw: 3, hufflepuff:1},
-        { optionText: 'Not likely', gryffindor: 1, slytherin: 0, ravenclaw: 3, hufflepuff: 3 },
-        { optionText: 'Never he\'s evil i\'d rather die', gryffindor: 4, slytherin: 0, ravenclaw: 2, hufflepuff: 0},
+        { optionText: 'Join him he\'s the most powerful', gryffindor: 0, slytherin: 4, ravenclaw: 1, hufflepuff: 2 },
+        { optionText: 'Probably join him to not die', gryffindor: 0, slytherin: 2, ravenclaw: 2, hufflepuff:2},
+        { optionText: 'Not likely', gryffindor:1, slytherin: 0, ravenclaw: 1, hufflepuff: 3 },
+        { optionText: 'Never he\'s evil i\'d rather die', gryffindor: 4, slytherin: 0, ravenclaw: 2, hufflepuff: 0}
     ],
 },
 
-]
+];
